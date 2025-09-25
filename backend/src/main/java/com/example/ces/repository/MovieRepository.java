831009399a5dc2c943314d0797a1e5a896e92f58
cb94147 <153537730+cb94147@users.noreply.github.com>
2025-09-25 19:07:41 -0400

    Fix MovieRepository.java after rebase conflict markers

--- backend/src/main/java/com/example/ces/repository/MovieRepository.java
+++ backend/src/main/java/com/example/ces/repository/MovieRepository.java
@@ -1,23 +1,16 @@
 package com.example.ces.repository;
 
 import com.example.ces.model.Movie;
-<<<<<<< HEAD
-=======
 import java.util.List;
 
->>>>>>> 478bdf1 (Linked to the DB and changed genre to an array)
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, String> {
-<<<<<<< HEAD
-
-}
-=======
+    
     List<Movie> findByTitleContainingIgnoreCase(String title);
-
     List<Movie> findByGenresIgnoreCase(String genre);
 
 }
->>>>>>> 478bdf1 (Linked to the DB and changed genre to an array)
+
