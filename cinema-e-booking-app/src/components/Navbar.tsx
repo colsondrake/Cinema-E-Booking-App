f5956bb1ea1a9f9e20832e33a8cfca8d3de329e5
Colson Drake <colsonjdrake@gmail.com>
2025-12-05 14:01:32 -0500

    Remove test button

--- cinema-e-booking-app/src/components/Navbar.tsx
+++ cinema-e-booking-app/src/components/Navbar.tsx
@@ -104,7 +104,7 @@ const Navbar = () => {
 					CINEMAGIC
 				</Link>
 				<AuthNavMenu />
-				<button 
+				{/* <button 
 					className="border border-blue-600 bg-blue-600 text-white hover:bg-blue-500 py-1.5 px-4 rounded ml-2 cursor-pointer"
 					onClick={() => {
 						console.log("ACCOUNT: ", account);
@@ -113,7 +113,7 @@ const Navbar = () => {
 						console.log("SHOWTIME: ", showtime);
 					}}>
 						Console Log
-				</button>
+				</button> */}
 			</div>
 		</div>
 	);
